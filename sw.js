252d6f88d2649453eb3b9f9231300bd89e4c96de
leecreizer <52846016+leecreizer@users.noreply.github.com>
2026-06-26 21:05:12 +0900

    Show live ticker price in PWA analysis

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'coinharness-mobile-v3';
+const CACHE = 'coinharness-mobile-v4';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon.png', './icon.svg'];
 
 self.addEventListener('install', event => {
